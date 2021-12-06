f14e40d0371caebb7bb57256e7fad4152dca229c
mayahoro <90589528+mayahoro@users.noreply.github.com>
2021-12-06 13:12:41 -0500

    Add files via upload

---
+++ Final_Project.py
@@ -0,0 +1,17 @@
+#Group Name: M&J 
+#Final Project
+#Covid API and Weather API
+
+import sqlite3
+import os
+import matplotlib.pyplot as plt
+
+#Function for getting data from Covid API
+def get_covid_data():
+    pass
+
+#Function for getting data from Weather API
+def get_weather_data():
+    pass
+
+#Function for finding 
